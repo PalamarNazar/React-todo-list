6e2aabd7afd47c7441fe6614cc2a4c85f98d24bb
Nazar Palamar <nazarpalamar46@gmail.com>
2026-01-18 21:38:24 +0100

    Update ModalWin.jsx

--- src/components/modalWin/ModalWin.jsx
+++ src/components/modalWin/ModalWin.jsx
@@ -1,4 +1,4 @@
-import AddTaskForm from '../AddTaskForm/AddTaskForm.jsx';
+import AddTaskForm from '../addTaskForm/AddTaskForm.jsx';
 import { useContext, useRef } from 'react';
 import { UiContext } from '../../context/UiContext.jsx';
 import { useFocusTrap } from '../../hooks/useFocusTrap.js';
