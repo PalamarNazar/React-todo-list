cac824ccc65afc20f39367d08a8901ddbb9d0d63
Nazar <nazarpalamar46@gmail.com>
2026-01-18 20:36:44 +0100

    routes debag commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import TaskPage from "./Pages/TaskPage/TaskPage"
 
 const App = () => {
   const routes = {
-    '/': ListPage,
+    '/React-todo-list/': ListPage,
     '/tasks/:id': TaskPage,
     '*': () => <h1>Error, Page not Found...</h1>,
   }
